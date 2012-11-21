dd9cac3a784113b03b302242d6eb794930a515f3
Mario Minutolo <mario.minutolo@gmail.com>
2012-11-21 23:45:29 +0100

    Creazione programma di esempio per realy

---
+++ relay/relay.ino
@@ -0,0 +1,15 @@
+int Relay = 2;
+  
+void setup()
+{
+  pinMode(13, OUTPUT);         //Set Pin13 as output
+  digitalWrite(13, HIGH);     //Set Pin13 High
+  pinMode(Relay, OUTPUT);     //Set Pin3 as output
+}
+void loop()
+{
+          digitalWrite(Relay, HIGH);   //Turn off relay
+          delay(2000);
+          digitalWrite(Relay, LOW);    //Turn on relay
+          delay(2000);
+}
